58d4f6b440900b2467530df9fc42e85c2eee112b
elninocavalcanti10 <acn2011@outlook.com>
2021-07-22 18:28:01 -0300

    Ensure LoginRouter returns 401 when invalid credentials

--- src/presentation/routes/login-router.js
+++ src/presentation/routes/login-router.js
@@ -17,5 +17,8 @@ module.exports = class LoginRouter {
       return HttpResponse.badRequest('Password')
     }
     this.authUseCase.auth(email, password)
+    return {
+      statusCode: 401
+    }
   }
 }
